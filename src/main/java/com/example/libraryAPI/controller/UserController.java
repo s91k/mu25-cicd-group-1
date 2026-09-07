7d42bc5a811ace08edd834ed66aaf2020ebaa4f0
arezooShahgaldi <arezooshahgaldi-lang>
2026-09-07 21:44:30 +0200

    Add User entity, repository, service, controller, tests and exception handling

---
+++ src/main/java/com/example/libraryAPI/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.libraryAPI.controller;
+
+import com.example.libraryAPI.dto.UserRequest;
+import com.example.libraryAPI.model.User;
+import com.example.libraryAPI.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody UserRequest request) {
+        return userService.create(request);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable Long id, @RequestBody UserRequest request) {
+        return userService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+}
--- src/main/java/com/example/libraryAPI/model/User.java
+++ src/main/java/com/example/libraryAPI/model/User.java
@@ -1,22 +1,9 @@
 package com.example.libraryAPI.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-
-@Entity
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank
     private String firstName;
-
-    @NotBlank
     private String lastName;
 
     public User() {
